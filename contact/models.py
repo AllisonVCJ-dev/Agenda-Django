bd1c22ecc2d969d0203d9590fe716a491c300d9c
AllisonVCJ-dev <allisonvaldocorrea14@gmail.com>
2024-04-18 20:43:43 -0300

    Configuração de Widgets, validação e adição de campos

--- contact/models.py
+++ contact/models.py
@@ -1,23 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import  User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
-# category (foreign key), show (boolean), picture (imagem),
+# category (foreign key), show (boolean), picture (imagem)
 # owner (foreign key)
 
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-
     name = models.CharField(max_length=50)
-    
+
     def __str__(self) -> str:
         return self.name
-
+    
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -26,14 +25,14 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank = True, upload_to='pictures/%Y/%m/')
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
-        Category, 
+        Category,
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
-    owner =  models.ForeignKey(
-        User, 
+    owner = models.ForeignKey(
+        User,
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
