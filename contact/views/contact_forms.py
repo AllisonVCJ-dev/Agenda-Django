1501b6e2648abbe369631baf6b956f12f2fbb698
AllisonVCJ-dev <allisonvaldocorrea14@gmail.com>
2024-04-19 14:42:56 -0300

    Validações

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,10 +4,11 @@ from contact.forms import ContactForm
 from contact.models import Contact
 
 def create(request):
+    
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
@@ -17,7 +18,7 @@ def create(request):
         if form.is_valid():
             contact = form.save()
             return redirect('contact:update', contact_id=contact.pk)
-
+   
         return render(
             request,
             'contact/create.html',
@@ -35,13 +36,15 @@ def create(request):
     )
 
 def update(request, contact_id):
+    
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
@@ -51,12 +54,13 @@ def update(request, contact_id):
         if form.is_valid():
             contact = form.save()
             return redirect('contact:update', contact_id=contact.pk)
-
+       
         return render(
             request,
             'contact/create.html',
             context
         )
+    
     context = {
         'form': ContactForm(instance=contact),
         'form_action': form_action,
@@ -69,15 +73,17 @@ def update(request, contact_id):
     )
 
 def delete(request, contact_id):
+    
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    
     confirmation = request.POST.get('confirmation', 'no')
-
+    
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
-
+   
     return render(
         request,
         'contact/contact.html',
